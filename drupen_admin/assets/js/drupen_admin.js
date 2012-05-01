451e10a0f9a106ed81e95083ebb9c5be94c023c4
Eriksen Costa <eriksencosta@gmail.com>
2012-05-01 02:06:57 -0300

    Fixed JS issue in switch editor link.

--- drupen_admin/assets/js/drupen_admin.js
+++ drupen_admin/assets/js/drupen_admin.js
@@ -4,10 +4,16 @@ Drupal.behaviors.drupen_admin = function(context) {
 
   if (isNodeForm) {
     $('form#node-form a').each(function() {
+      var id = $(this).attr('id').trim();
       var href = $(this).attr('href').trim();
 
+      // Won't open a new window for the switch editor link.
+      if  (id === 'switch_edit-body') {
+        return;
+      }
+
       // URLs not starting with javascript:
-      if (href != 'javascript:') {
+      if (href !== 'javascript:') {
         $(this).attr('target', 'blank');
       }
     });
